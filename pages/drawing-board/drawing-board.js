fc1d8f165c9257d7032f118ac88fc0a2f80dae54
zhangxh1023 <zhangxh1023@gmail.com>
2023-08-24 19:06:18 +0800

    ui

--- pages/drawing-board/drawing-board.js
+++ pages/drawing-board/drawing-board.js
@@ -1,6 +1,13 @@
 import MinaTouch from '../../utils/touch'
-import { Actions } from '../../utils/actions/actions';
-import { Pair } from '../../utils/actions/pair';
+import {
+  Actions
+} from '../../utils/actions/actions';
+import {
+  Pair
+} from '../../utils/actions/pair';
+
+const TOP_BAR_HEIGHT = 60;
+const DPR = wx.getSystemInfoSync().pixelRatio;
 
 /**
  * @type { Actions | null }
@@ -11,7 +18,11 @@ let actionId = 0;
 Page({
 
   data: {
-
+    enbaleRollbackBtn: false,
+    enbaleRedoBtn: false,
+    enablePencilBtn: true,
+    enableRubberBtn: true,
+    isEditing: false,
   },
 
   /**
@@ -21,12 +32,13 @@ Page({
     const that = this;
     new MinaTouch(this, 'touch1', {
       //会创建this.touch1指向实例对象
-      touchStart: function () { },
+      touchStart: function () {},
       touchMove: function (evt) {
+        if (!that.data.isEditing) return;
         if (evt.touches && evt.touches.length === 1) {
           // 画线条
           const x = evt.touches[0].pageX;
-          const y = evt.touches[0].pageY;
+          const y = evt.touches[0].pageY - TOP_BAR_HEIGHT / DPR;
           if (actions) {
             actions.execHandWriting({
               id: actionId,
@@ -45,13 +57,13 @@ Page({
         actionId = ++actionId % Number.MAX_VALUE;
         actions.endHandWriting();
       },
-      touchCancel: function () { },
-      multipointStart: function (evt) { }, //一个手指以上触摸屏幕触发
-      multipointEnd: function () { }, //当手指离开，屏幕只剩一个手指或零个手指触发(一开始只有一根手指也会触发)
-      tap: function () { }, //点按触发，覆盖下方3个点击事件，doubleTap时触发2次
-      doubleTap: function () { }, //双击屏幕触发
-      longTap: function () { }, //长按屏幕750ms触发
-      singleTap: function () { }, //单击屏幕触发，包括长按
+      touchCancel: function () {},
+      multipointStart: function (evt) {}, //一个手指以上触摸屏幕触发
+      multipointEnd: function () {}, //当手指离开，屏幕只剩一个手指或零个手指触发(一开始只有一根手指也会触发)
+      tap: function () {}, //点按触发，覆盖下方3个点击事件，doubleTap时触发2次
+      doubleTap: function () {}, //双击屏幕触发
+      longTap: function () {}, //长按屏幕750ms触发
+      singleTap: function () {}, //单击屏幕触发，包括长按
       rotate: function (evt) {
         //evt.angle代表两个手指旋转的角度
       },
@@ -64,7 +76,7 @@ Page({
           actions.scale({
             id: actionId,
             scale,
-            center: new Pair((touch1.pageX + touch2.pageX) / 2, (touch1.pageY + touch2.pageY) / 2),
+            center: new Pair((touch1.pageX + touch2.pageX) / 2, (touch1.pageY + touch2.pageY - TOP_BAR_HEIGHT / DPR - TOP_BAR_HEIGHT / DPR) / 2),
           });
         }
       },
@@ -91,12 +103,11 @@ Page({
         // 画板 canvas
         const boardCanvas = res[0].node;
         const boardCtx = boardCanvas.getContext('2d');
-        const dpr = wx.getSystemInfoSync().pixelRatio;
         const originWidht = res[0].width;
         const originHeight = res[0].height;
-        boardCanvas.width = originWidht * dpr;
-        boardCanvas.height = originHeight * dpr;
-        boardCtx.scale(dpr, dpr);
+        boardCanvas.width = originWidht * DPR;
+        boardCanvas.height = originHeight * DPR;
+        boardCtx.scale(DPR, DPR);
         actions = new Actions({
           boardCtx,
         });
@@ -143,6 +154,61 @@ Page({
    */
   onShareAppMessage() {
 
+  },
+
+  touchStartRollbackBtn() {
+    this.setData({
+      enbaleRollbackBtn: false
+    });
+  },
+
+  touchEndRollbackBtn() {
+    this.setData({
+      enbaleRollbackBtn: true
+    });
+  },
+
+  touchStartRedoBtn() {
+    this.setData({
+      enbaleRedoBtn: false
+    });
+  },
+
+  touchEndRedoBtn() {
+    this.setData({
+      enbaleRedoBtn: true
+    });
+  },
+
+  touchStartPencilBtn() {
+    this.setData({
+      enablePencilBtn: false,
+    });
+  },
+
+  touchEndPencilBtn() {
+    this.setData({
+      enablePencilBtn: true,
+      isEditing: true,
+    });
+  },
+
+  touchStartRubberBtn() {
+    this.setData({
+      enableRubberBtn: false,
+    });
+  },
+
+  touchEndRubberBtn() {
+    this.setData({
+      enableRubberBtn: true,
+    });
+  },
+
+  exitEditModel() {
+    this.setData({
+      isEditing: false,
+    });
   },
 
 })
